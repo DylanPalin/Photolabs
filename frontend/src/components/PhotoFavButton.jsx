5536aef7fb8da200646f03a3d309c4cafb4eba70
Dylan Palin <dylanpalin@gmail.com>
2024-01-12 14:31:18 -0700

    added fav button with state management

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,13 +1,24 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState } from "react";
 
-import FavIcon from './FavIcon';
-import '../styles/PhotoFavButton.scss';
+import FavIcon from "./FavIcon";
+import "../styles/PhotoFavButton.scss";
 
 function PhotoFavButton() {
+  const [isFavorited, setIsfavorited] = useState(false);
+
+  const handleFavClick = useCallback(() => {
+    setIsfavorited(!isFavorited);
+  }, [isFavorited]);
+
   return (
     <div className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <button
+          onClick={handleFavClick}
+          className={isFavorited ? "favorited" : ""}
+        >
+          <FavIcon isFavorited={isFavorited} />
+        </button>
       </div>
     </div>
   );
