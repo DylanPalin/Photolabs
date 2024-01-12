26bfaf64d73d73211e02bd741210adf23690f32a
Dylan Palin <dylanpalin@gmail.com>
2024-01-11 19:35:47 -0700

    first commit

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,20 +2,14 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = ({photo}) => {
+  return (
+    <div>
+      <img src={photo.imageSource} alt={photo.username} />
+      <h2>{photo.username}</h2>
+      <p>{photo.location.city}, {photo.location.country}</p>
+    </div>
+  );
 };
 
 export default PhotoListItem;
